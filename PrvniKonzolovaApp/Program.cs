873d6663b3d4c6a090f601667b9e4f338eb17e84
Marek Bohanes <m.bohanes@gmail.com>
2022-04-25 16:23:28 +0200

    nedodělané - podmínka while

--- PrvniKonzolovaApp/Program.cs
+++ PrvniKonzolovaApp/Program.cs
@@ -1,45 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+Console.WriteLine("zadávejte čísla, po skončení stiskni klávesu q");
+ 
+int soucet = 0;
 
-
-string DenVTydnu(int vstup)
+while(!Console.ReadKey = "q")
 {
+   int vstup = int.Parse(Console.ReadLine());
+   soucet += vstup;
 
-    switch (vstup)
-    {
-        case 1:
-            return "pondělí";
-            break;
-        case 2:
-            return "úterý";
-            break;
-        case 3:
-            return "středa";
-            break;
-        case 4:
-            return "čtvrtek";
-            break;
-        case 5:
-            return "pátek";
-            break;
-        case 6:
-            return "sobota";
-            //vikend = true;
-            break;
-        case 7:
-            return "neděle";
-            //   vikend = true;
-            break;
-        default:
-            return "chyba";
-            break;
-
-    }
 }
 
-string den = DenVTydnu(2);
-Console.WriteLine(den);
+Console.WriteLine(soucet);
+
+
+
+
+//string DayOfWeek(DenVTydnu day)
+//{
+
+//    switch (day)
+//    {
+//        case DenVTydnu.PONDELI:
+//            return "pondělí";
+//        case DenVTydnu.UTERY:
+//            return "úterý";
+//        case DenVTydnu.STREDA:
+//            return "středa";
+//        case DenVTydnu.CTVRTEK:
+//            return "čtvrtek";
+//        case DenVTydnu.PATEK:
+//            return "pátek";
+//        case DenVTydnu.SOBOTA:
+//            return "sobota";
+//        case DenVTydnu.NEDELE:
+//            return "neděle";
+//        default:
+//            return "chyba";
+
+//    }
+//}
+
+//string den = DayOfWeek(2);
+//Console.WriteLine(den);
 
 
 //int Suma(int a, int b)
@@ -53,3 +57,15 @@ Console.WriteLine(den);
 
 //int sum = Suma(10, 15);
 //Console.WriteLine(sum);
+
+
+//enum DenVTydnu
+//{
+//    PONDELI,
+//    UTERY,
+//    STREDA,
+//    CTVRTEK,
+//    PATEK,
+//    SOBOTA,
+//    NEDELE
+//}
