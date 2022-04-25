c0cacc5e182decc72fa410d3dd38e1abfe54a04d
Marek Bohanes <m.bohanes@gmail.com>
2022-04-25 13:31:25 +0200

    přiřazení dne v týdnu

--- PrvniKonzolovaApp/Program.cs
+++ PrvniKonzolovaApp/Program.cs
@@ -1,23 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Zadej číslo A:");
-double Cislo_A = double.Parse(Console.ReadLine());
+Console.WriteLine("Zadej číslo dne v týdnu:");
+int vstup = int.Parse(Console.ReadLine());
+string vystup;
 
-Console.WriteLine("Zadej číslo B:");
-double Cislo_B = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Zadej číslo B:");
-double Cislo_C = double.Parse(Console.ReadLine());
+if (vstup == 1)
+{ vystup = "pondělí"; }
+else if (vstup == 2)
+{ vystup = "úterý"; }
+else if (vstup == 3)
+{ vystup = "středa"; }
+else if (vstup == 4)
+{ vystup = "čtvrtek"; }
+else if (vstup == 5)
+{ vystup = "pátek"; }
+else if (vstup == 6)
+{ vystup = "sobota"; }
+else if (vstup == 7)
+{ vystup = "neděle"; }
+else { vystup = "špatné číslo"; }
 
-double nejvetsi_cislo = 0;
-
-if (Cislo_A > Cislo_B && Cislo_A > Cislo_C)
-{
-   nejvetsi_cislo = Cislo_A;
-}
-else if (Cislo_B > Cislo_A && Cislo_B > Cislo_C) 
-    {
-    nejvetsi_cislo = Cislo_B;
-    }
-else nejvetsi_cislo = Cislo_C;
-
-Console.WriteLine($"Největší je číslo {nejvetsi_cislo}");
+Console.WriteLine($"Zadal jsi {vstup}, což je {vystup}.");
