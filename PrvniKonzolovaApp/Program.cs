6dcbf88da99f0309e0715202753d5bcb80408738
Marek Bohanes <m.bohanes@gmail.com>
2022-04-25 14:22:33 +0200

    cyklus součet čísel zadaných uživatelem

--- PrvniKonzolovaApp/Program.cs
+++ PrvniKonzolovaApp/Program.cs
@@ -1,39 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Zadej číslo dne v týdnu:");
+//Console.WriteLine("Zadej číslo dne v týdnu:");
+//int vstupni_hodnota = int.Parse(Console.ReadLine());
+//string den;
+//bool vikend = false;
+
+//switch (vstupni_hodnota)
+//{
+//    case 1:
+//        den = "pondělí";
+//        break;
+//    case 2:
+//        den = "úterý";
+//        break;
+//    case 3:
+//        den = "středa";
+//        break;
+//    case 4:
+//        den = "čtvrtek";
+//        break;
+//    case 5:
+//        den = "pátek";
+//        break;
+//    case 6:
+//        den = "sobota";
+//        vikend = true;
+//        break;
+//    case 7:
+//        den = "neděle";
+//        vikend = true;
+//        break;
+//    default:
+//        den = "chyba";
+//        break;
+
+//}
+
+//Console.WriteLine($"Zadal jsi {vstupni_hodnota}, což je {den} a to { vikend ? "je víkend.":"není víkend."});
+
+
+
+Console.WriteLine("Zadej číslo");
 int vstup = int.Parse(Console.ReadLine());
-string vystup;
-bool vikend_bool = false;
-string vikend;
+int vystup = 0; 
 
-switch (vstup)
+for (int i = 1; i <= vstup; i++)
 {
-    case 1:
-        vystup = "pondělí";
-        break;
-    case 2:
-        vystup = "úterý";
-        break;
-    case 3:
-        vystup = "středa";
-        break;
-    case 4:
-        vystup = "čtvrtek";
-        break;
-    case 5:
-        vystup = "pátek";
-        break;
-    case 6:
-        vystup = "sobota";
-        vikend_bool = true;
-        break;
-    case 7:
-        vystup = "neděle";
-        vikend_bool = true;
-        break;
-    default: 
-        vystup = "chyba";
-        break;
-
+    vystup = vystup + i;
 }
-if (vikend_bool) { vikend = "je víkend"; } else { vikend = "není víkend"; };
-Console.WriteLine($"Zadal jsi {vstup}, což je {vystup} a to {vikend}");
+
+Console.WriteLine($"součet je {vystup}");
