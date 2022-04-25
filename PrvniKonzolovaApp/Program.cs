87381cbd132c8df4ceb74de21629f957bb3d53e7
Marek Bohanes <m.bohanes@gmail.com>
2022-04-25 11:21:01 +0200

    prohození hodnoty proměnné

---
+++ PrvniKonzolovaApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 10;
+
+int b = 20;
+
+int c = 0;
+Console.WriteLine("původní a=" + a + ", původní b=" + b);
+
+c = a;
+a = b;
+b = c;
+
+Console.WriteLine("nové a=" + a + ", nové b=" + b);
