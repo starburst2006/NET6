ea38eb066f78c2816c0592c48796ac5a40c38d9a
Marek Bohanes <m.bohanes@gmail.com>
2022-04-25 13:53:34 +0200

    doplněn víkend

--- PrvniKonzolovaApp/Program.cs
+++ PrvniKonzolovaApp/Program.cs
@@ -2,7 +2,8 @@
 Console.WriteLine("Zadej číslo dne v týdnu:");
 int vstup = int.Parse(Console.ReadLine());
 string vystup;
-
+bool vikend_bool = false;
+string vikend;
 
 switch (vstup)
 {
@@ -23,14 +24,16 @@ switch (vstup)
         break;
     case 6:
         vystup = "sobota";
+        vikend_bool = true;
         break;
     case 7:
         vystup = "neděle";
+        vikend_bool = true;
         break;
     default: 
         vystup = "chyba";
         break;
 
 }
-
-Console.WriteLine($"Zadal jsi {vstup}, což je {vystup}.");
+if (vikend_bool) { vikend = "je víkend"; } else { vikend = "není víkend"; };
+Console.WriteLine($"Zadal jsi {vstup}, což je {vystup} a to {vikend}");
