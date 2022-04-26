bd916068aee672e0fe9fa709cb1344bd881a9f80
Marek Bohanes <m.bohanes@gmail.com>
2022-04-26 10:27:31 +0200

    třída a konstruktory

--- PrvniKonzolovaApp/Program.cs
+++ PrvniKonzolovaApp/Program.cs
@@ -5,11 +5,16 @@
 //person1.DateOfBirdth = new DateTime(1986, 03, 08);
 
 
+// pokud zadám konstruktor, už nemůžu zadat takto, popřípadě musím vložit prázdný konstruktor
+//_2D bod1 = new _2D();
+//bod1.X = 20;
+//bod1.Y = 10;
 
-_2D bod1 = new _2D();
-bod1.X = 20;
-bod1.Y = 10;
+//_2D bod2 = new _2D();
+//bod2.X = 30;    
+//bod2.Y = 15;
 
-_2D bod2 = new _2D();
-bod2.X = 30;    
-bod2.Y = 15;    
+
+_2D bod3 = new _2D(12, 32);
+
+Console.WriteLine($"X: {bod3.X} y: {bod3.Y}");
