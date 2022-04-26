6b7dea77eec01ddd2345a5694181bfb1071242f8
Marek Bohanes <m.bohanes@gmail.com>
2022-04-26 15:53:03 +0200

    vytvoreno pole kradených SPZ

--- PrvniKonzolovaApp/Data/StolenCheck.cs
+++ PrvniKonzolovaApp/Data/StolenCheck.cs
@@ -9,16 +9,22 @@ namespace PrvniKonzolovaApp.Data
 {
     internal class StolenCheck
     {
+        public static string[] StolenCars = { "1B18855", "1B28855", "1B38855", "1B48855" };
+        
         /// <summary>
         /// zkontroluje zda není SPZ v databázi kradených SPZ
         /// </summary>
         /// <returns>Vrátí true pokud je auto kradené, jinak false</returns>
         public static bool GetRegisterInfo(Car auto)
         {
+            foreach(string kradenaSPZ in StolenCars)
+            {
+            if (auto.SPZ == kradenaSPZ) 
+                    return true; 
             
-            if (auto.SPZ == "1B18855") { return true; }
-            else { return false; }
-            
+            }
+             return false;
+           
         }
     }
 }
--- PrvniKonzolovaApp/Program.cs
+++ PrvniKonzolovaApp/Program.cs
@@ -1,59 +1,9 @@
 ﻿using PrvniKonzolovaApp.Model;
 using PrvniKonzolovaApp.Data;
 
-//Person person1 = new Person("Marek","Bohanes");
-//person1.DateOfBirdth = new DateTime(1986, 03, 08);
-
-//Console.WriteLine(person1);
 
 
-
-
-
-// pokud zadám konstruktor, už nemůžu zadat takto, popřípadě musím vložit prázdný konstruktor
-//_2D bod1 = new _2D();
-//bod1.X = 20;
-//bod1.Y = 10;
-
-//_2D bod2 = new _2D();
-//bod2.X = 30;    
-//bod2.Y = 15;
-
-
-//_2D bod3 = new _2D(1, 0);  // Pomocí konstruktoru
-
-//Console.WriteLine(bod3.GetQuadrant());
-
-//Console.WriteLine($"X: {bod3.X} y: {bod3.Y}");
-
-//bod3.Print();
-
-
-
-
-
-//Obsah Obsah1 = new Obsah(20, 10);
-
-//Console.WriteLine(Obsah1.VypocitejObsah());
-
-
-
-//for (int i = 1; i <= 10; i++)
-//{
-//    Console.WriteLine(RandomPointGenerator.GetRandomPoint());
-//}
-
-//Console.WriteLine(RandomPointGenerator.GetCount());
-
-//_2D bod1 = new _2D(100, 10);  // Pomocí konstruktoru
-//_2D bod2 = new _2D(10, 10);  // Pomocí konstruktoru
-
-
-
-//Console.WriteLine(Point2DMath.WhitchIsBigger(bod1, bod2));
-
-
-Car Auto1 = new Car("1B88855",  "červená",  10000,  "Škoda");
+Car Auto1 = new Car("1B58855",  "červená",  10000,  "Škoda");
 
 bool result = StolenCheck.GetRegisterInfo(Auto1);
 
