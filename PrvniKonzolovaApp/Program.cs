70d7be1e91bede3d61a4e018175c18a444e8df26
Marek Bohanes <m.bohanes@gmail.com>
2022-04-27 14:33:55 +0200

    Počet osob žijící v Praze

--- PrvniKonzolovaApp/Program.cs
+++ PrvniKonzolovaApp/Program.cs
@@ -10,11 +10,11 @@ for (int i = 0; i < 50; i++)
     lide.Add(RandomPersonGenerator.GetRandomPerson());
 }
 
-var result = lide.OrderBy(osoba => osoba.DateOfBirth).Where(osoba => osoba.Address.City == "Brno").First();
+var result = lide.Where(osoba => osoba.Address.City == "Praha").Count();
 
 //foreach (Person osoba in result)
 //{
 //    Console.WriteLine(osoba + " " + osoba.DateOfBirth.ToString("yyyy-MM-dd"));
 //}
 
-Console.WriteLine(result + " " + result.DateOfBirth.ToString("yyyy-MM-dd") + " " + result.Address.City);
+Console.WriteLine(result + " " );
