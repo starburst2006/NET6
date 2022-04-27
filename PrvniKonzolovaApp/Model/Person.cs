cd1f152baf2c7454469ffb4c6a0bbe0136d045ea
Marek Bohanes <m.bohanes@gmail.com>
2022-04-27 10:19:35 +0200

    přidáno do osob komplexní proměnná adresa

--- PrvniKonzolovaApp/Model/Person.cs
+++ PrvniKonzolovaApp/Model/Person.cs
@@ -16,6 +16,7 @@ namespace PrvniKonzolovaApp.Model
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirdth { get; set; }
+        public Address Address { get; set; }
 
         public override string ToString()
         {
