bf8fee7de2a47f60e3fdc5ec46758e6d7b750ac6
Marek Bohanes <m.bohanes@gmail.com>
2022-04-27 11:32:51 +0200

    LINQ hledání prvku

--- PrvniKonzolovaApp/Program.cs
+++ PrvniKonzolovaApp/Program.cs
@@ -4,19 +4,27 @@ using PrvniKonzolovaApp.Data;
 
 int[] cisla = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256 };
 
-var result = cisla.Where(cislo => cislo > 0);
+//čísla větší než 0
+//var result2 = cisla.Where(cislo => cislo > 0);
 
 //// foreach (var cislo in result)
 //{
 //    Console.WriteLine(cislo);
 //}
-
+//podmínky -50 - +50
 //var result2 = cisla.Where(cislo => cislo >= -50 && cislo <=50);
 
-var result2 = cisla.OrderByDescending(cislo => cislo);
+//řazení
+//var result2 = cisla.OrderByDescending(cislo => cislo);
+
+// iterování výsledkem
+//foreach (var cislo in result2)
+//{
+//    Console.WriteLine(cislo);
+//}
 
 
-foreach (var cislo in result2)
-{
-    Console.WriteLine(cislo);
-}
+// najít prvek
+var i = cisla.Where(cislo => cislo == 127).First();
+
+
