c1409bf39c642dc9058dd19393af2115512ada63
Marek Bohanes <m.bohanes@gmail.com>
2022-04-27 11:20:00 +0200

    LINQ operátor where

--- PrvniKonzolovaApp/Program.cs
+++ PrvniKonzolovaApp/Program.cs
@@ -2,15 +2,11 @@
 using PrvniKonzolovaApp.Data;
 
 
-Car Auto1 = new Car("1B18855",  "černá metalýza",  10000,  "Škoda");
-Person majitel1 = new Person("Marek", "Bohanes");
-//Address address1 = new Address("Mikulovská 8", "Brno", 62800, "Česká republika");
+int[] cisla = { 2, 43, 22, 12, 54, -5, 41, 127, 90, -25, -71, 45 };
 
-Auto1.Majitel = majitel1;
-//Auto1.Majitel.Address = address1;
+var result = cisla.Where(cislo => cislo > 0);
 
-Console.WriteLine($"Auto s SPZ {Auto1.SPZ} vlastní {Auto1.Majitel.FirstName}");
-
-Auto1.Majitel.Address.City = "Brno";
-
-Console.WriteLine(Auto1.Majitel.Address);
+foreach (var cislo in result)
+{
+    Console.WriteLine(cislo);
+}
