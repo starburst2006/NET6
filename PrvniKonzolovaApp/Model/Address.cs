85a0a80b1e70db5b743df0b507ece952c3e75b2e
Marek Bohanes <m.bohanes@gmail.com>
2022-04-27 09:56:27 +0200

    Vytvořena třída pro adresu

---
+++ PrvniKonzolovaApp/Model/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrvniKonzolovaApp.Model
+{
+    internal class Address
+    {
+        public Address(string street, string city, int postalCode, string country)
+        {
+            Street = street;
+            City = city;
+            PostalCode = postalCode;
+            Country = country;
+        } 
+
+
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Street}, {City} {PostalCode}, {Country}";
+        }
+    }
+}
